fef04f014d2db6d08fec668329722bcd68cd5c67
s2000promax <s2000promax@gmail.com>
2023-01-07 22:26:08 +0400

    feat: add PR workflow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn github actions
         </a>
       </header>
     </div>
